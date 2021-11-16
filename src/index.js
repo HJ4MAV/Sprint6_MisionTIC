7d8da5d66f02837250f870d78dc13108f817b361
Maicon Avendaño Ochoa <av0ch@protonmail.com>
2021-11-15 20:11:36 -0500

    Actualizacion final app

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+var invocation = new XMLHttpRequest();
+var url = 'http://localhost:3002/';
+var body = '<?xml version="1.0"?><person><name>Arun</name></person>';
+
+function callOtherDomain(){
+  if(invocation)
+    {
+      invocation.open('POST', url, true);
+      invocation.setRequestHeader('X-PINGOTHER', 'pingpong');
+      invocation.setRequestHeader('Content-Type', 'application/xml');
+      invocation.send(body);
+    }
+}
